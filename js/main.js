6409a5d75268cbe7f42225fb0ec21f697caf2844
Louis MD <louis@margot-duclot.fr>
2020-01-23 23:20:31 +0100

    full screen, more platforms & end game logic

--- js/main.js
+++ js/main.js
@@ -11,9 +11,12 @@ var platforms = document.getElementsByClassName("platform") ;
 
 var state = {
     collisionDetected: false,
-    jumpHeight: 240,
+    jumpHeight: 250,
     score: 1,
+    lost: true,
 }
+
+window.scrollTo(0, 1447)
 
 window.onkeydown = function(e) {
     keyState[e.code] = true;
@@ -26,12 +29,12 @@ window.onkeyup = function(e) {
 function moveGrandma(direction) {
 
     if (direction === "left") {
-        grandma.left -= 8;
+        grandma.left -= 10;
         grandma.isFacing = "left"
     }
 
     if (direction === "right") {
-        grandma.left += 8;
+        grandma.left += 10;
         grandma.isFacing = "right"
     }
 }
@@ -48,7 +51,6 @@ function jump() {
 function fall() {
 
     // console.log("Fall function called"); 
-
     
     if (state.collisionDetected) {
         // console.log(`Grandma's fall was stopped by an object at ${grandma.bottom}`);
@@ -57,7 +59,7 @@ function fall() {
     } else if (!state.collisionDetected && grandma.bottom > 0) {
         // console.log(state.collisionDetected)
         grandma.isFalling = true;
-        grandma.bottom -= 5;
+        grandma.bottom -= 8;
         // console.log("Oh no! Grandma is falling...");
     } else if (grandma.bottom <= 0) {
         // console.log("Grandma reached the bottom!")
@@ -113,7 +115,31 @@ function renderScore() {
     if (grandma.bottom > state.score) {
         state.score = grandma.bottom;
     }
-    document.getElementById("score").innerHTML = `Score: ${state.score}`;
+    document.getElementById("score").innerHTML = `Score: ${state.score - 242}`;
+}
+
+function scrollScreen() {
+    if (document.getElementById("grandma").getBoundingClientRect().y < 0) {
+        window.scrollBy(0,-200);
+    }
+}
+
+function endGame() {
+    if (document.getElementById("grandma").getBoundingClientRect().y > 900) {
+        window.scrollTo(0, 2500);
+        grandma.bottom = 0;
+        renderGrandma();
+        if(!alert(`You lost! Your score is ${state.score}. Not bad for a grandma! \nClick "Ok" to play again.`)){
+        window.location.reload();}
+    }
+
+    if (grandma.bottom > 3000) {
+        grandma.bottom = 3200;
+        document.getElementById("grandma").getBoundingClientRect().x = 600;
+        renderGrandma();
+        if(!alert(`You win! Your score is ${state.score}. Congrats!!`)){
+            window.location.reload();}
+    }
 }
 
 function renderEverything() {
@@ -124,12 +150,18 @@ function renderEverything() {
     fall();
     renderGrandma();
     renderScore();
+    scrollScreen();
+    endGame();
     requestAnimationFrame(renderEverything);
 }
 
 setInterval(() => {
     jump();
-}, 250);
+}, 100);
+
+(function initScroll(){
+    window.scrollTo(0, 2500);
+})();
 
 requestAnimationFrame(renderEverything);
 
